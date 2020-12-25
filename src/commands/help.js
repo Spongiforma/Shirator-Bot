dc4d8f3cd24fd8e807440810ded26e2a2505e647
Spongiforma <56219994+Spongiforma@users.noreply.github.com>
2020-12-25 16:11:06 +0800

    change dm to general message

--- src/commands/help.js
+++ src/commands/help.js
@@ -13,16 +13,16 @@ module.exports = {
 			data.push('List of commands:');
 			data.push(commands.map(command => command.name).join(', '));
 			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-
-			return message.author.send(data, { split: true })
-				.then(() => {
-					if (message.channel.type === 'dm') return;
-					message.reply('I\'ve sent you a DM with all my commands!');
-				})
-				.catch(error => {
-					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
-					message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
-				});
+			return message.channel.send(data);
+			// return message.author.send(data, { split: true })
+			// 	.then(() => {
+			// 		if (message.channel.type === 'dm') return;
+			// 		message.reply('I\'ve sent you a DM with all my commands!');
+			// 	})
+			// 	.catch(error => {
+			// 		console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+			// 		message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+			// 	});
 		} else{
 			const name = args[0].toLowerCase();
 			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.include(name));
